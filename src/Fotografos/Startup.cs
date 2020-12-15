d9ee9926c03cefacf52bbb26df316be6510ff641
Adriano Vega <adrianovega@hotmail.com>
2020-12-15 20:09:56 +0100

    Register the dbcontext and the managers

--- src/Fotografos/Startup.cs
+++ src/Fotografos/Startup.cs
@@ -1,7 +1,9 @@
+using Fotografos.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,6 +23,16 @@ namespace Ejercicio
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            // Configure the DBContext.
+            string connection = this.Configuration.GetConnectionString("DefaultConnection");
+
+            services.AddDbContext<FotomixContext>(
+                options => options.UseSqlite(connection)
+            );
+
+            services.AddFotografosServices();
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
