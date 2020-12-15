15f8d347e9c3f72105a1b8a62fe7e50cda8bc146
Adriano Vega <adrianovega@hotmail.com>
2020-12-15 20:04:48 +0100

    Created the Logic project and implemented a basic ApplicationManager

---
+++ src/logic/LogicServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+namespace Microsoft.Extensions.DependencyInjection
+{
+    using Fotografos.Logic.Managers;
+
+    public static class LogicServiceCollectionExtensions
+    {
+        /// <summary>
+        ///     A service collection extension method for registering the managers from
+        ///     Fotografos's logic.
+        /// </summary>
+        /// <param name="services">The service collection.</param>
+        /// <returns>The service collection with the configured managers.</returns>
+        public static IServiceCollection AddFotografosServices(this IServiceCollection services)
+        {
+            services.AddScoped<ApplicationManager>();
+
+            return services;
+        }
+    }
+}
---
+++ src/logic/Managers/ApplicationManager.cs
@@ -0,0 +1,46 @@
+namespace Fotografos.Logic.Managers
+{
+    using System.Collections.Generic;
+    using Fotografos.Domain;
+    using Fotografos.Persistence;
+
+    public class ApplicationManager
+    {
+        private readonly FotomixContext context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationManager"/> class.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public ApplicationManager(FotomixContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Application> GetApplications()
+        {
+            return this.context.Applications;
+        }
+
+        public Application GetApplication(long id)
+        {
+            return this.context.Applications.Find(id);
+        }
+
+        public Application CreateApplication(Application application)
+        {
+            if (application.Date == default || string.IsNullOrEmpty(application.EquimentDescription) ||
+                string.IsNullOrEmpty(application.Resume) || application.PhotographerId == 0)
+            {
+                return null;
+            }
+
+            // TODO: Make async.
+            this.context.Applications.Add(application);
+            this.context.SaveChanges();
+            this.context.Entry(application).GetDatabaseValues();
+
+            return application;
+        }
+    }
+}
