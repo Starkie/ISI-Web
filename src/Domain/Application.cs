f5a2262f0d1fd82f98a40119b76e9a67255fba4c
Adriano Vega <adrianovega@hotmail.com>
2020-12-15 20:04:14 +0100

    Changed the type of the Date

--- src/Domain/Application.cs
+++ src/Domain/Application.cs
@@ -6,7 +6,7 @@ namespace Fotografos.Domain
     public partial class Application
     {
         public long Id { get; set; }
-        public string Date { get; set; }
+        public DateTime Date { get; set; }
         public string EquimentDescription { get; set; }
         public string Resume { get; set; }
         public long? PhotographerId { get; set; }
