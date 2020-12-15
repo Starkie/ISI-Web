476f12cc8c00ec726c2e9caff0f200feb7c34e9b
Adriano Vega <adrianovega@hotmail.com>
2020-12-15 20:12:23 +0100

    Changed the namespace of the startup classs

--- src/Fotografos/Startup.cs
+++ src/Fotografos/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Ejercicio
+namespace Fotografos.Server
 {
     public class Startup
     {
